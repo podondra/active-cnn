8efbe12bd901e8ff6fecf799f79dc7b21193bc57
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-10-13 07:27:38 +0200

    Complete preprocessing section

--- active_cnn/data.py
+++ active_cnn/data.py
@@ -1,3 +1,4 @@
+import h5py
 import pandas as pd
 
 
@@ -8,3 +9,10 @@ def get_ondrejov_dataset(dataset_file):
     ondrejov_flux = ondrejov_dataset.iloc[:, 12:].values
     ondrejov_labels = ondrejov_dataset['label'].values
     return ondrejov_ids, ondrejov_flux, ondrejov_labels
+
+
+def get_lamost_dataset(dataset_file):
+    lamost_collection = h5py.File(dataset_file)['lamost_dr2']
+    lamost_ids = lamost_collection['filenames'][:]
+    lamost_flux = lamost_collection['spectra'][...]
+    return lamost_ids, lamost_flux
