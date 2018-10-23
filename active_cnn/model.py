ce01bcf1e277ceb299ba740502f39caab7012438
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2018-10-23 10:56:44 +0200

    Change architecture and add run code

--- active_cnn/model.py
+++ active_cnn/model.py
@@ -1,4 +1,5 @@
 import keras
+import h5py
 
 
 class CNN:
@@ -20,16 +21,19 @@ class CNN:
             keras.layers.Dropout(0.5),
             keras.layers.Dense(512, activation='relu'),
             keras.layers.Dropout(0.5),
-            keras.layers.Dense(1, activation='sigmoid')
+            # 3 neurouns in ouput layer
+            keras.layers.Dense(3, activation='sigmoid')
             ])
-        self.cnn.compile(loss='binary_crossentropy', optimizer='adam')
+        self.cnn.compile(loss='categorical_crossentropy', optimizer='adam')
         self.callback = keras.callbacks.EarlyStopping(
-                monitor='loss', min_delta=10e-5, patience=50
+                monitor='loss', min_delta=10e-4, patience=10,
+                restore_best_weights=True
                 )
 
     def train(self, X, y):
         self.cnn.fit(
                 X.reshape(-1, 1, 140, 1), y,
+                batch_size=64,
                 epochs=1000, callbacks=[self.callback]
                 )
 
@@ -38,3 +42,37 @@ class CNN:
 
     def save(self, filepath):
         self.cnn.save(filepath)
+
+
+def get_training_data(it, hdf5):
+    with h5py.File(hdf5, 'r+') as f:
+        it_gr = f['iteration_{:02}'.format(it)]
+        X_tr = it_gr['X_train'][...]
+        y_tr = it_gr['y_train'][...]
+        X = it_gr['X'][...]
+    return X_tr, y_tr, X
+
+
+def save_predictions(it, hdf5, y_pred):
+    with h5py.File(hdf5, 'r+') as f:
+        it_gr = f['iteration_{:02}'.format(it)]
+        dt = it_gr.create_dataset('y_pred', y_pred.shape, y_pred.dtype)
+        dt[...] = y_pred
+
+
+def learning(it, hdf5):
+    X_tr, y_tr, X = get_training_data(it, hdf5)
+    X_tr_bal, y_tr_bal = balance(X_tr, y_tr)
+    # build the model and train it
+    cnn = model.CNN()
+    cnn.train(X_tr_bal, y_tr_bal)
+    # save network's weights
+    cnn.save('data/cnn-bu-it-{:02}.hdf5'.format(it))
+    # get and save predictions
+    y_pred = cnn.predict(X)
+    save_predictions(it, hdf5, y_pred)
+
+
+if __name__ == '__main__':
+    import sys
+    learning(int(sys.argv[1]), sys.argv[2])
