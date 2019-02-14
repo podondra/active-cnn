205a52b1d600c44b9e0ce049659a6b753055e742
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2019-02-14 13:44:42 +0100

    Add label to a plot function

--- active_cnn/plot.py
+++ active_cnn/plot.py
@@ -8,7 +8,7 @@ def plot_ondrejov_spectrum(spectrum_id, ondrejov, ax):
     wavelengths = np.linspace(6519, 6732, num=140)
     index = ondrejov[0] == spectrum_id
     fluxes = ondrejov[1][index].reshape(140)
-    ax.plot(wavelengths, fluxes)
+    ax.plot(wavelengths, fluxes, label='vacuum and convolved')
 
 
 def preview_lamost_spectrum(filename, prediction, save=False):
